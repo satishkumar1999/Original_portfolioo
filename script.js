5f56c56b2bd431097ce84035c7ccda0f7fd2135e
satish kumar <raj715255@gmail.com>
2025-02-12 07:40:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text",{
+    strings: ["Frontend Developer","Backend Developer","Fullstack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+document.querySelector('.bx-link-external').addEventListener('click', function() {
+    console.log('Icon clicked!');
+});
